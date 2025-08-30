786b98a77ea3f3b467df40a9b1aa05b0ddd9ce9a
Mahendra <innovatormahe@gmail.com>
2025-08-30 21:08:01 +0530

    Initial Commit

--- serverless/bin/aws_projects.ts
+++ serverless/bin/aws_projects.ts
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { AwsProjectsStack } from '../lib/aws_projects-stack';
+import { LambdaStack } from '../lib/lambda-stacks';
+import { S3Stack } from '../lib/s3-stack';
+import { DynamoDBStack } from '../lib/DynamoDB-stack';
 
 const app = new cdk.App();
-new AwsProjectsStack(app, 'AwsProjectsStack', {
+new LambdaStack(app, 'LambdaStack');
+new S3Stack(app, 'S3Stack');
+new DynamoDBStack(app, 'DynamoDBStack');
+/* new AwsProjectsStack(app, 'AwsProjectsStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -16,5 +21,5 @@ new AwsProjectsStack(app, 'AwsProjectsStack', {
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html 
+});*/
